1e7173883886b1b466b0bc76fc188cce2e16c5a1
Daniel Wheeler <daniel.wheeler2@gmail.com>
2018-05-16 13:53:59 -0400

    Add build for ipywidgets

--- shell.nix
+++ shell.nix
@@ -6,8 +6,9 @@ let
     rev = "5e58e59a945cd3ce3c39d88cc4618d4f426cd030";
     sha256 = "1s5hmgyxjfs5sgb13sx0rmnkgccmnkn6x64hhxrzjj1m71l5nkyw";
   };
+  nixpkgs = import <nixpkgs> {};
   pkgs = nixpkgs.pkgs;
-  nixpkgs = import "${download}/fipy-py3/nixpkgs_version.nix";
+  # nixpkgs = import "${download}/fipy-py3/nixpkgs_version.nix";
   fipy = import "${download}/fipy-py3/fipy.nix" { inherit nixpkgs; };
   skfmm = import "${download}/fipy-py3/skfmm.nix" { inherit nixpkgs; };
   nbval = import "${download}/fipy-py3/nbval.nix" { inherit nixpkgs; };
@@ -26,8 +27,13 @@ in
       nixpkgs.python36Packages.pip
       nixpkgs.python36Packages.matplotlib
       nixpkgs.python36Packages.flake8
+      nixpkgs.python36Packages.ipywidgets
       fipy
       skfmm
       nbval
     ];
+    shellHook = ''
+      jupyter nbextension install --py widgetsnbextension --user
+    '';
+
   }
